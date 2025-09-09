c02063d9edd7bddcd7372fe3447c98ba55179f6b
jmarcelotse <jmarcelotse@hotmail.com>
2025-09-09 09:51:57 -0300

    add aula13

--- aws_db_instance.tf
+++ aws_db_instance.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "bia" {
   allow_major_version_upgrade = null
   apply_immediately           = null
   auto_minor_version_upgrade  = true
-  availability_zone           = "us-east-1a"
+  # availability_zone           = "us-east-1a"
   backup_retention_period     = 0
   backup_window               = "09:05-09:35"
   ca_cert_identifier          = "rds-ca-rsa2048-g1"
@@ -61,4 +61,13 @@ resource "aws_db_instance" "bia" {
   timezone                              = null
   username                              = "postgres"
   vpc_security_group_ids                = [aws_security_group.bia_db.id]
+  db_subnet_group_name                  = aws_db_subnet_group.bia.name
+}
+
+resource "aws_db_subnet_group" "bia" {
+  name       = "bia-subnet-group"
+  subnet_ids = [local.subnet_zona_a, local.subnet_zona_b]
+  tags = {
+    Name = "BIA subnet group"
+  }
 }
--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   vpc_id        = "vpc-03c4e823f9cde5442"
-  subnet_zona_a = ""
+  subnet_zona_a = "subnet-08efca2de6c4fa678"
   subnet_zona_b = "subnet-0cc1e65e576bc7e50"
 }
