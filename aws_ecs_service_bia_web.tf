34fd231f37af22f481ef8a169d9c4b68c6c22ca6
jmarcelotse <jmarcelotse@hotmail.com>
2025-09-10 10:14:08 -0300

    Launch Template v3 - network interfaces otimizadas

--- aws_ecs_service_bia_web.tf
+++ aws_ecs_service_bia_web.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_service" "bia" {
     field = "attribute:ecs.availability-zone"
   }
 
-  deployment_minimum_healthy_percent = 0
+  deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 100
 
   # lifecycle {
--- aws_launch_template.tf
+++ aws_launch_template.tf
@@ -4,10 +4,17 @@ data "aws_ssm_parameter" "ecs_node_ami" {
 
 # resource "aws_launch_template_ecs" "ecs_ec2" {
 resource "aws_launch_template" "ecs_ec2" {
-  name_prefix            = "cluster-bia-web-"
-  image_id               = data.aws_ssm_parameter.ecs_node_ami.value
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.bia_ec2.id]
+  name_prefix   = "cluster-bia-web-"
+  image_id      = data.aws_ssm_parameter.ecs_node_ami.value
+  instance_type = "t3.micro"
+  #vpc_security_group_ids = [aws_security_group.bia_ec2.id]
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.bia_ec2.id]
+    #ipv6_address_count = 1
+  }
+
   iam_instance_profile { arn = aws_iam_instance_profile.ecs_node.arn }
   monitoring { enabled = false }
 
